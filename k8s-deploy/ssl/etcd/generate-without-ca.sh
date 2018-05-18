11bb8c34204845cf75d6f1703fd0d6a7b434c75a
mhcvs2 <merzhong0501@163.com>
2018-05-18 15:59:04 +0800

    add

--- k8s-deploy/ssl/etcd/generate-without-ca.sh
+++ k8s-deploy/ssl/etcd/generate-without-ca.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 des_dir=/etc/etcd/ssl
+etcd_ips="109.105.30.155,109.105.30.156,109.105.1.209,109.105.1.208,109.105.30.157,109.105.1.253,109.105."
 
 mkdir -p ${des_dir}
 cp ./*.json ${des_dir}
@@ -10,6 +11,7 @@ cfssl gencert \
 -ca=etcd-ca.pem \
 -ca-key=etcd-ca-key.pem \
 -config=ca-config.json \
+-hostname="" \
 -profile=kubernetes \
 etcd-csr.json | cfssljson -bare etcd
 
