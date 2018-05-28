967842f155dcfa5777c9bc3b4cb6de327f282b89
mhcvs2 <merzhong0501@163.com>
2018-05-28 17:25:27 +0800

    add

--- k8s/commit-job/build.sh
+++ k8s/commit-job/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-docker build -t registry.bst-1.cns.bstjpc.com:5000/docker-commit-push:18.03.0-ce .
+docker build -t registry.bst-1.cns.bstjpc.com:5000/docker-commit-push:20180528 .
 
 docker push registry.bst-1.cns.bstjpc.com:5000/docker-commit-push:18.03.0-ce
